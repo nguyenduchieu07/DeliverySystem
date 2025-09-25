7850d3dca05216bef8f0bdaf96dc91f4ef8af279
Trinh Truong Giang <giangtthe170641@fpt.edu.vn>
2025-09-25 16:37:27 +0700

    feat(view): add view services

---
+++ PresentationLayer/Areas/Stores/Controllers/ServiceController.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Abstractions.IRepositories;
+using DataAccessLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PresentationLayer.Areas.Stores.Models;
+
+namespace PresentationLayer.Areas.Stores.Controllers
+{
+    [Area("Stores")]
+    public class ServiceController : Controller
+    {
+        private readonly IBaseRepository<Service,Guid> _serviceRepository;
+        private readonly Guid _storeId = Guid.Parse("22222222-2222-2222-2222-222222222222"); // demo
+        public ServiceController(IBaseRepository<Service,Guid> baseRepository)
+        {
+            _serviceRepository = baseRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var query = await _serviceRepository.FindAll(e => e.StoreId == _storeId,
+                e => e.Category
+                ).Select(s => new ServiceListItemVM
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    CategoryName = s.Category != null ? s.Category.Name : null,
+                    Unit = s.Unit,
+                    BasePrice = s.BasePrice,
+                    IsActive = s.IsActive,
+                    IsPublished = EF.Property<bool>(s, "IsActive") == true,
+                    CreatedAt = s.CreatedAt
+                }).ToListAsync();
+
+            return View(query);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var vm = new ServiceEditVM { StoreId = _storeId, IsActive = true, IsPublished = false };
+            return View(vm);
+        }
+    }
+}
---
+++ PresentationLayer/Areas/Stores/Models/PriceTierVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PresentationLayer.Areas.Stores.Models
+{
+    public class PriceTierVM
+    {
+        public Guid? Id { get; set; }
+        public Guid ServiceId { get; set; }
+        [Required] public DateTime ValidFrom { get; set; } = DateTime.UtcNow.Date;
+        public DateTime? ValidTo { get; set; }
+        [Range(0.01, 999999999)] public decimal Price { get; set; }
+        public int? MinQty { get; set; }
+        public int? MaxQty { get; set; }
+    }
+}
