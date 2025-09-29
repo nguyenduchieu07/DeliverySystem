c918aae8ecadf22b5b9ec0fbe54cfba64827acd2
Pham Ngoc Phong <phonghip2607@gmail.com>
2025-09-29 15:15:11 +0700

    update(entity): add relation ship for warehouse and address

--- DataAccessLayer/Migrations/20250929081429_update_warehouse_slot.Designer.cs
+++ DataAccessLayer/Migrations/20250929081429_update_warehouse_slot.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(DeliverySytemContext))]
-    partial class DeliverySytemContextModelSnapshot : ModelSnapshot
+    [Migration("20250929081429_update_warehouse_slot")]
+    partial class update_warehouse_slot
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
