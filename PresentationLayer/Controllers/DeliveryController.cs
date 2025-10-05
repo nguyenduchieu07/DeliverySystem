47d4fadeb107af8e132d3ca2f2b0dedab4453b20
Tuanlmhe <tuanlmhe172616@fpt.edu.vn>
2025-10-05 20:08:25 +0700

    Send Mail and order

--- PresentationLayer/Controllers/DeliveryController.cs
+++ PresentationLayer/Controllers/DeliveryController.cs
@@ -1,25 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
-namespace PresentationLayer.Controllers
-{
-    [Authorize] // Yêu cầu đăng nhập
-    public class DeliveryController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace PresentationLayer.Controllers { [Authorize] // Yêu cầu đăng nhập
+ public class DeliveryController : Controller 
+    { 
+        public IActionResult Index() { 
+            return View(); 
+        } 
+        public IActionResult Orders() {
             return View();
-        }
-
-        public IActionResult Orders()
-        {
-            return View();
-        }
-
-        // Thêm các action khác nếu cần
-        public IActionResult BookDelivery()
-        {
-            return View();
-        }
+        } // Thêm các action khác nếu cần
+         public IActionResult BookDelivery() { 
+            return View(); 
+        } 
     }
 }
--- PresentationLayer/Models/ProfileViewModel.cs
+++ PresentationLayer/Models/ProfileViewModel.cs
@@ -1,4 +1,7 @@
-﻿using DataAccessLayer.Entities;
+﻿// PresentationLayer/Models/ProfileViewModel.cs
+using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace PresentationLayer.Models
 {
@@ -8,5 +11,17 @@ namespace PresentationLayer.Models
         public List<Address> Addresses { get; set; } = new List<Address>();
         public List<Order> Orders { get; set; } = new List<Order>();
         public Address NewAddress { get; set; } = new Address();
+
+        // Thêm thuộc tính cho booking kho
+        public Address PickupAddress { get; set; } = new Address();
+        public Address DropoffAddress { get; set; } = new Address();
+        public DateTime? DeliveryDate { get; set; }
+        public DateTime? PickupDate { get; set; }
+        public string Note { get; set; }
+        public string SelectedVehicle { get; set; } = "motorbike";
+        public float? DistanceKm { get; set; }
+        public int? EtaMinutes { get; set; }
+        public bool UseStoredPickupAddress { get; set; }
+        public bool UseStoredDropoffAddress { get; set; }
     }
 }
