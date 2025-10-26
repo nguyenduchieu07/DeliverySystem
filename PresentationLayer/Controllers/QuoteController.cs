258a3bddfc8ab874b9abf3603e754ceb6a9b1a37
Trinh Truong Giang <giangtthe170641@fpt.edu.vn>
2025-10-27 00:42:49 +0700

    feat(update)

---
+++ PresentationLayer/Controllers/QuoteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class QuoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
