f0b2778ccce7bd867e70b6e4fea7a82833a38f3c
hoanxhe <hoanxhe170914@fpt.edu.vn>
2025-10-28 11:17:48 +0700

    Refactor admin feedback to store management
    Removed FeedbackController and its views, migrating feedback management under the new StoreController. Added new views for store listing, details, and feedbacks, and updated shared layout and sidebar to reflect these changes. This refactor centralizes store and feedback management for better maintainability and user experience.

---
+++ PresentationLayer/Areas/Admin/Controllers/StoreController.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLayer.Abstractions.IRepositories;
+using DataAccessLayer.Entities;
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Areas.Admin.Controllers;
+
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class StoreController : Controller
+{
+    private readonly IBaseRepository<Store, Guid> _storeRepository;
+    private readonly IFeedbackRepository _feedbackRepository;
+    public StoreController(IBaseRepository<Store, Guid> storeRepository, IFeedbackRepository feedbackRepository)
+    {
+        _storeRepository = storeRepository;
+        _feedbackRepository = feedbackRepository;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var stores = await _storeRepository.GetAllAsync();
+        return View(stores);
+    }
+    
+    public async Task<IActionResult> Detail(Guid storeId)
+    {
+        var vm = new AdminStoreDetailViewModel();
+        if (storeId == Guid.Empty)
+            return NotFound();
+
+        var feedbacks = await _feedbackRepository.GetAllFeedbackByStoreIdAsync(storeId);
+        vm.ReviewCount = feedbacks.Count();
+        
+        var store = await _storeRepository.GetByIdAsync(storeId);
+        vm.Store = store;
+        
+        return View(vm);
+    }
+    
+    public async Task<IActionResult> GetFeedbacksByStoreAsync(Guid storeId)
+    {
+        var feedbacks = await _feedbackRepository.GetAllFeedbackByStoreIdAsync(storeId);
+
+        return View(feedbacks);
+    }
+}
---
+++ PresentationLayer/Models/AdminStoreDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLayer.Entities;
+
+namespace PresentationLayer.Models;
+
+public class AdminStoreDetailViewModel
+{
+    public int ReviewCount { get; set; }
+
+    public Store Store { get; set; }
+}
